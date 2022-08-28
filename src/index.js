a45e3c017e880223525649090dc2b21864139fa7
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-28 20:20:38 +0300

    Getting elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // write your code here
+
+let cardinfo;
+function cardRender(card){
+    // console.log(card)
+    cardinfo = card
+    cardTitleTag = document.querySelector('#card-title'),
+    cardImageTag = document.querySelector('#card-image'),
+    cardLikesTag = document.querySelector('#like-count'),
+    cardLikesBtnTag = document.querySelector('#like-button'),
+    cardListTag = document.querySelector('#comments-list'),
+    cardCommentFormTag = document.querySelector('#comment-form'),
+    cardCommentInputTag = document.querySelector('#comment'),
+
+    
+
+};
+
